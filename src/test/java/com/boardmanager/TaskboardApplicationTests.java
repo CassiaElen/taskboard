98255a0c8376133619f8945793a821900dfc35ab
CassiaElen <cassiaelen6002@gmail.com>
2025-04-19 14:09:48 -0300

    estrutura básica

---
+++ src/test/java/com/boardmanager/TaskboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
